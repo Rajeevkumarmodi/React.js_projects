4ef2e22697426a04ec2108a12686348d4716ce51
Rajeevkumarmodi <125949049+Rajeevkumarmodi@users.noreply.github.com>
2023-12-23 13:32:35 +0530

    cart page setup

--- React.js_eCommerce/src/App.jsx
+++ React.js_eCommerce/src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/header/Header";
 import HomePage from "./pages/homePage/HomePage";
 import Footer from "./components/footer/Footer";
 import SingleProductPage from "./pages/singleProductPage/SingleProductPage";
+import Cart from "./pages/cart/Cart";
 function App() {
   return (
     <div>
@@ -12,7 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/product/:id" element={<SingleProductPage />} />
-          <Route path="/cart" element={<p>This is cart page</p>} />
+          <Route path="/cart" element={<Cart />} />
           <Route path="/order" element={<p>This is order page</p>} />
           <Route path="/profile" element={<p>This is profile page</p>} />
         </Routes>
---
+++ React.js_eCommerce/src/pages/cart/Cart.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Cart() {
+  return (
+    <div className="mt-[120px] md:mx-[80px] mx-[10px]">
+      <div className="flex justify-between">
+        <div>left</div>
+        <div>right</div>
+      </div>
+    </div>
+  );
+}
+
+export default Cart;
