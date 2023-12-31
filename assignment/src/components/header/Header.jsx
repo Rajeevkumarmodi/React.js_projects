b9560c30c595cad5e825f775cd237a61b750a596
Rajeevkumarmodi <125949049+Rajeevkumarmodi@users.noreply.github.com>
2023-12-31 11:18:15 +0530

    login and logout functionality

--- assignment/src/components/header/Header.jsx
+++ assignment/src/components/header/Header.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./header.css";
 
 function Header() {
-  return <div>this is header</div>;
+  const navigate = useNavigate();
+  const token = localStorage.getItem("authToken");
+
+  function logoutHandler() {
+    localStorage.removeItem("authToken");
+    navigate("/login");
+  }
+  return (
+    <div className="header">
+      <div className="links">
+        {token ? (
+          <button onClick={logoutHandler}>Logout</button>
+        ) : (
+          <Link to="/login">Login</Link>
+        )}
+        <Link to="/">Home</Link>
+      </div>
+      <div>Cart</div>
+    </div>
+  );
 }
 
 export default Header;
--- assignment/src/pages/login/Login.jsx
+++ assignment/src/pages/login/Login.jsx
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import "./login.css";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
-  return <div>this is login page</div>;
+  const navigate = useNavigate();
+  const [inputValue, setInputValue] = useState({
+    username: "",
+    password: "",
+  });
+
+  function changeHandler(e) {
+    const { name, value } = e.target;
+    setInputValue({ ...inputValue, [name]: value });
+  }
+
+  // =================================login logic=================================
+
+  async function loginHandler(e) {
+    try {
+      e.preventDefault();
+      const res = await fetch("https://dummyjson.com/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(inputValue),
+      });
+
+      // username:kminchelle
+      // password : 0lelplR
+
+      const jsonData = await res.json();
+      if (jsonData.hasOwnProperty("email")) {
+        localStorage.setItem("authToken", jsonData.token);
+        navigate("/");
+      } else {
+        alert(jsonData.message);
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  return (
+    <form className="loginPage">
+      <h3>Login Page</h3>
+      <div className="inputBox">
+        <label htmlFor="username">Username: </label>
+        <input
+          onChange={changeHandler}
+          name="username"
+          value={inputValue.username}
+          type="text"
+          id="username"
+        />
+      </div>
+      <div className="inputBox">
+        <label htmlFor="password">Password: </label>
+        <input
+          onChange={changeHandler}
+          name="password"
+          value={inputValue.password}
+          type="password"
+          id="password"
+        />
+      </div>
+      <button onClick={loginHandler}>Login</button>
+    </form>
+  );
 }
 
 export default Login;
