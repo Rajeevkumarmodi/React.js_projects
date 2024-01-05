cbe36c60e339596b9344a4c4594a2e5e4e835f38
Rajeevkumarmodi <125949049+Rajeevkumarmodi@users.noreply.github.com>
2024-01-05 19:30:44 +0530

    heroBanner

--- film ki duniya/src/components/heroBanner/HeroBanner.jsx
+++ film ki duniya/src/components/heroBanner/HeroBanner.jsx
@@ -1,14 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useApiFetch from "../../hooks/useApiFetch";
+import { useSelector } from "react-redux";
 
 function HeroBanner() {
   const navigate = useNavigate();
   const [background, setBackground] = useState("");
   const [query, setQuery] = useState("");
-
+  const { url } = useSelector((state) => state.home);
   const { data, loading } = useApiFetch("/movie/upcoming");
-  console.log(data);
+
+  useEffect(() => {
+    const bg =
+      url?.backdrop +
+      data?.results[Math.floor(Math.random() * 20)].backdrop_path;
+
+    console.log(bg);
+    setBackground(bg);
+  }, [data]);
+
   //=====================   handel search==========================
   function handelSearch(event) {
     if (event.key === "Enter" && query.length > 0) {
@@ -17,18 +27,29 @@ function HeroBanner() {
   }
 
   return (
-    <div>
+    <div
+      className="md:h-[400px] h-[350px] bg-cover bg-center relative flex flex-col gap-3 items-center justify-center"
+      style={{
+        backgroundImage: `url(${background})`,
+      }}
+    >
       <div>
-        <h2>Welcome.</h2>
-        <p>Millions of movies, TV shows and people to discover. Explore now.</p>
+        <h2 className="font-bold text-5xl pb-3">Welcome.</h2>
+        <p className="font-bold md:text-3xl text-xl">
+          Millions of movies, TV shows and people to discover. Explore now.
+        </p>
       </div>
-      <div>
+      <div className="relative">
         <input
+          className="w-[90vw] py-2 rounded-full pl-4"
           type="text"
+          placeholder="Search for a movie | tv show..."
           onChange={(e) => setQuery(e.target.value)}
           onKeyUp={handelSearch}
         />
-        <button>Search</button>
+        <button className="absolute right-0 bg-teal-500 px-6 py-2 rounded-full">
+          Search
+        </button>
       </div>
     </div>
   );
