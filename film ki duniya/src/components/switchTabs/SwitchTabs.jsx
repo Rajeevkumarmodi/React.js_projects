23b36846a957fdf6569b31f6389b45fed29dc00d
Rajeevkumarmodi <125949049+Rajeevkumarmodi@users.noreply.github.com>
2024-01-08 13:08:18 +0530

    switch tabs

---
+++ film ki duniya/src/components/switchTabs/SwitchTabs.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+function SwitchTabs() {
+  const [activeTab, setActiveTab] = useState("Day");
+  const tabs = ["Day", "Wieek"];
+  return (
+    <div className="flex w-[200px] bg-white text-black font-bold border-2 rounded-full">
+      {tabs.map((item, index) => {
+        return (
+          <div
+            onClick={() => setActiveTab(item)}
+            className={`${
+              activeTab === item &&
+              "bg-gradient-to-br from-green-400 to-blue-600 text-white font-bold"
+            } rounded-full w-[100px] py-1 text-center cursor-pointer`}
+            key={index}
+          >
+            {item}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default SwitchTabs;
