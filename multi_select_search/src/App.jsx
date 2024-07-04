06f4f833358be531bb850c7e93205b09a08589a8
Rajeevkumarmodi <125949049+Rajeevkumarmodi@users.noreply.github.com>
2024-07-04 13:02:20 +0530

    added show selected user and remove use

--- multi_select_search/src/App.jsx
+++ multi_select_search/src/App.jsx
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [selectedUser, setSelectedUser] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResult, setSearchResult] = useState([]);
+  const [uniqueUserId, setUniqueUserId] = useState(new Set());
 
   useEffect(() => {
     async function fetchData() {
@@ -14,32 +16,75 @@ function App() {
       setSearchResult(result.users);
     }
 
-    if (!searchTerm) return;
-    fetchData();
+    if (searchTerm) {
+      fetchData();
+    } else {
+      setSearchResult([]);
+      return;
+    }
   }, [searchTerm]);
+
+  function handleSelectUser(user) {
+    setSelectedUser([...selectedUser, user]);
+    setUniqueUserId((prevSet) => new Set(prevSet).add(user.id));
+    setSearchResult([]);
+    setSearchTerm("");
+  }
+
+  function handleRemoveSelectedUser(user) {
+    const filterUser = selectedUser.filter((us) => us.id !== user.id);
+    setSelectedUser(filterUser);
+    uniqueUserId.delete(user.id);
+  }
 
   return (
     <div>
       <div className="search-box">
-        <input
-          type="text"
-          value={searchTerm}
-          placeholder="search hear"
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        {/* search result */}
-        {searchResult.length > 0 && (
-          <div className="search-result">
-            {searchResult?.map((user) => (
-              <div key={user.id} className="user">
-                <img src={user.image} alt="user image" />
-                <p>
-                  {user.firstName} {user.lastName}
-                </p>
-              </div>
-            ))}
-          </div>
-        )}
+        {/* display sleseted user */}
+        <div className="delected-users">
+          {selectedUser?.map((user) => (
+            <div
+              key={user.id}
+              onClick={() => handleRemoveSelectedUser(user)}
+              className="delected-user"
+            >
+              <img src={user.image} alt="user image" />
+              <p>
+                {user.firstName} {user.lastName}
+              </p>
+              <span>×</span>
+            </div>
+          ))}
+        </div>
+
+        <div className="input">
+          <input
+            type="text"
+            value={searchTerm}
+            placeholder="search hear"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          {/* search result */}
+          {searchResult.length > 0 && (
+            <div className="search-result">
+              {searchResult?.map(
+                (user) =>
+                  !uniqueUserId.has(user.id) && (
+                    <div
+                      onClick={() => handleSelectUser(user)}
+                      key={user.id}
+                      className="user"
+                    >
+                      <img src={user.image} alt="user image" />
+                      <p>
+                        {user.firstName} {user.lastName}
+                      </p>
+                    </div>
+                  )
+              )}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
