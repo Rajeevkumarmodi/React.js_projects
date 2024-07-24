48948ef504b43a4f19db8e4e6fe7214f0f36acef
Rajeevkumarmodi <125949049+Rajeevkumarmodi@users.noreply.github.com>
2024-07-24 15:08:47 +0530

    added search food and restaurant functionality

--- swiggy_clone/src/pages/Search.jsx
+++ swiggy_clone/src/pages/Search.jsx
@@ -1,44 +1,58 @@
-import React, { useState, useEffect } from "react";
-import { IoMdSearch } from "react-icons/io";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import SearchResultResCard from "../components/SearchResultResCard";
+import SearchSuggestionsCard from "../components/SearchSuggestionsCard";
+import {
+  Link,
+  Navigate,
+  useNavigate,
+  useNavigation,
+  useSearchParams,
+} from "react-router-dom";
+import { AiOutlineSearch, AiOutlineLeft } from "react-icons/ai";
+import { MdOutlineClear } from "react-icons/md";
 
-function Search() {
+const Search = () => {
   const [searchText, setSearchText] = useState("");
-  const [initialFoods, setInitialFoods] = useState([]);
+  const [foods, setFood] = useState([]);
   const [suggestions, setSuggestions] = useState([]);
+  const [searchByRes, setSearchByRes] = useState([]);
+  const [isDish, setIsDish] = useState("");
+
+  const navigate = useNavigate();
+  const [searchParam] = useSearchParams();
+  const searchName = searchParam.get("query");
+
+  const handleClick = (name, type) => {
+    setSearchText(name);
+    setIsDish(type);
+  };
 
   useEffect(() => {
     getSearchItemFoods();
   }, []);
 
-  useEffect(() => {
-    let timer = setTimeout(() => {
-      if (searchText.length > 2) {
-        getSearchSuggestions();
-      } else {
-        setSuggestions([]);
-      }
-    }, 1000);
-
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [searchText]);
-
-  function getNameFromPopularCusines(id) {
-    console.log("id", id);
-  }
-
   const getSearchItemFoods = async () => {
     const data = await fetch(
       "https://api.allorigins.win/raw?url=https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Flanding%2FPRE_SEARCH%3Flat%3D12.9715987%26lng%3D77.5945627"
     );
+
     const json = await data.json();
-    // console.log(json?.data?.cards[1]?.card?.card?.imageGridCards?.info);
-    setInitialFoods(json?.data?.cards[1]?.card?.card?.imageGridCards?.info);
+    setFood(json?.data?.cards[1]?.card?.card?.imageGridCards?.info);
   };
 
-  // food search function
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (searchText.length >= 2) {
+        getSearchSuggestions();
+      } else {
+        setSuggestions([]);
+        navigate("/search");
+      }
+    }, 1000);
+    return () => clearInterval(timer);
+  }, [searchText]);
+
   const getSearchSuggestions = async () => {
     const data = await fetch(
       "https://api.allorigins.win/raw?url=" +
@@ -46,73 +60,230 @@ function Search() {
           `https://www.swiggy.com/dapi/restaurants/search/suggest?lat=12.9715987&lng=77.5945627&str=${searchText}&trackingId=undefined`
         )
     );
+
     const json = await data.json();
-    console.log("suggesition", json?.data?.suggestions);
     setSuggestions(json?.data?.suggestions);
+  };
+
+  useEffect(() => {
+    if (searchName != null && searchText.length > 1) {
+      getSearchByRes();
+    }
+  }, [searchName]);
+
+  const getSearchByRes = async () => {
+    const data = await fetch(
+      "https://api.allorigins.win/raw?url=" +
+        encodeURIComponent(
+          `https://www.swiggy.com/dapi/restaurants/search/v3?lat=12.9715987&lng=77.5945627&str=${searchName}&trackingId=undefined&submitAction=SUGGESTION&queryUniqueId=bc9bbc65-5d8b-0aa5-bbdc-65b467e6afdb`
+        )
+    );
+    const json = await data.json();
+
+    if (isDish === "RESTAURANT") {
+      setSearchByRes(
+        json?.data?.cards[1]?.groupedCard?.cardGroupMap?.RESTAURANT
+      );
+    } else {
+      setSearchByRes(json?.data?.cards[1]?.groupedCard?.cardGroupMap?.DISH);
+    }
+  };
+
+  const foodItems = foods.slice(0, 11);
+  const getNameFromPopularCusines = (entityId) => {
+    const startIndex = entityId.indexOf("=") + 1;
+    const endIndex = entityId.length;
+
+    const extractedText = entityId.slice(startIndex, endIndex);
+
+    setSearchText(extractedText);
   };
 
   return (
     <>
       <div className="sticky top-20 left-0 pb-2 pt-12 bg-white z-2">
-        <div className="max-w-[800px] mx-auto px-5">
-          {/* search box */}
-          <div className="relative">
-            <input
-              onChange={(e) => setSearchText(e.target.value)}
-              className="w-full  border-[1.5px] border-gray-200 outline-none  leading-relaxed rounded-md py-3 px-5"
-              type="text"
-              placeholder="Search for restaurants and food "
-            />
-            <IoMdSearch className="absolute top-3 right-3 text-3xl" />
-          </div>
+        <div className="w-4/5 sm:w-3/4 h-12 xl:w-[860px] mx-auto border border-[#282c3f4d] border-b border-black rounded-md">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <div className="flex justify-center items-center w-full h-12 ">
+              {isDish && searchText && (
+                <Link
+                  to={"/search"}
+                  className="pl-2"
+                  onClick={() => {
+                    setSearchText("");
+                    setSearchByRes([]);
+                    setIsDish("");
+                  }}
+                >
+                  <AiOutlineLeft size={25} />
+                </Link>
+              )}
+              <input
+                type="text"
+                className="w-full border-none outline-none font-medium px-4 leading-relaxed "
+                placeholder="Search for restaurants and food"
+                value={searchText}
+                onChange={(e) => {
+                  setSearchText(e.target.value);
+                }}
+              />
+              {searchText == "" ? (
+                <button className="p-2 m-2">
+                  <AiOutlineSearch size={25} />
+                </button>
+              ) : (
+                <Link
+                  to={"/search"}
+                  className="p-2 m-2"
+                  onClick={(e) => {
+                    setSearchText("");
+                  }}
+                >
+                  <MdOutlineClear size={25} />
+                </Link>
+              )}
+            </div>
+          </form>
         </div>
       </div>
-      {searchText === "" ? (
-        <div className="max-w-[800px] mx-auto px-5">
-          <h2 className="font-bold text-xl">Popular Cuisines</h2>
-          <div className="flex flex-wrap">
-            {initialFoods?.map((foodItem) => (
-              <div
-                key={foodItem.id}
-                className="pr-2 cursor-pointer"
-                onClick={() => getNameFromPopularCusines(foodItem.entityId)}
-              >
-                <img
-                  src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/${foodItem.imageId}`}
-                  className="border-none max-w-[100px] "
-                  alt={`Image ${foodItem.id}`}
-                />
-              </div>
-            ))}
+      <div className="mx-12 sm:mx-20 md:mx-28 lg:mx-44 xl:mx-60 pt-3 pl-2">
+        {searchText === "" ? (
+          <div>
+            <h1 className="font-extrabold text-[1.43rem] text-[#3d4152]">
+              Popular Cuisines
+            </h1>
+            <div className="grid grid-cols-6 md:flex mt-2 pt-3 pr-4 pl-2 pb-6">
+              {foodItems.map((foodItem) => (
+                <div
+                  key={foodItem.id}
+                  className="pr-2 cursor-pointer"
+                  onClick={() => getNameFromPopularCusines(foodItem.entityId)}
+                >
+                  <img
+                    src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/${foodItem.imageId}`}
+                    className="border-none"
+                    alt={`Image ${foodItem.id}`}
+                  />
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-      ) : (
-        ""
-      )}
-      {/* show suggesition card */}
-      <ul>
-        {searchText !== "" && suggestions.length !== 0
-          ? suggestions.map((suggestion, index) => (
-              <li key={`suggestion_${index}`}>
-                <Link>
-                  <div className="max-w-[800px] my-3 p-1 mx-auto px-5 flex duration-300 hover:bg-gray-100">
-                    <img
-                      className="w-16 h-16 object-contain rounded"
-                      src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_112,h_112,c_fill/${suggestion.cloudinaryId}`}
-                      alt="Food-Image"
-                    />
-                    <div className="ml-4">
-                      <p className="text-sm">{suggestion.text}</p>
-                      <p className="text-sm">{suggestion.type}</p>
-                    </div>
-                  </div>
+        ) : (
+          ""
+        )}
+        {searchText === "" || searchName === searchText ? (
+          ""
+        ) : (
+          <ul>
+            {suggestions.map((suggestion, index) => (
+              <li key={index}>
+                <Link
+                  onClick={() => handleClick(suggestion.text, suggestion.type)}
+                  to={
+                    "/search?query=" +
+                    encodeURIComponent(suggestion.text).replace(/%20/g, "+")
+                  }
+                >
+                  <SearchSuggestionsCard {...suggestion} />
                 </Link>
               </li>
-            ))
-          : ""}
-      </ul>
+            ))}
+          </ul>
+        )}
+      </div>
+      <div className="mx-8 sm:mx-20 md:mx-28 lg:mx-44 xl:mx-60 pt-3 pl-2">
+        {(searchText === searchName && searchText === "") ||
+        searchName == null ? (
+          ""
+        ) : (
+          <ul>
+            <div className="flex gap-2 text-sm font-medium mb-1">
+              <h1
+                className={
+                  isDish === "RESTAURANT"
+                    ? "bg-black text-white py-2 px-4 rounded-3xl"
+                    : "p-2 border rounded-3xl"
+                }
+              >
+                Restaurants
+              </h1>
+              <h1
+                className={
+                  isDish === "DISH"
+                    ? "bg-black text-white py-2 px-4 rounded-3xl"
+                    : "p-2 border rounded-3xl"
+                }
+              >
+                Dishes
+              </h1>
+            </div>
+
+            <div className="bg-gray-100 py-4 px-2 md:px-4 z-2">
+              {isDish === "RESTAURANT" && searchByRes !== undefined ? (
+                <div>
+                  <div className="w-full md:w-1/2">
+                    {searchByRes.cards &&
+                    searchByRes.cards[0]?.card?.card?.info ? (
+                      <SearchResultResCard
+                        data={searchByRes.cards[0].card.card.info}
+                      />
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                  <h1 className="font-bold text-lg mt-8">More Restaurants</h1>
+                  <div className="grid grid-cols-1 md:grid-cols-2 pt-4">
+                    {searchByRes.cards &&
+                      searchByRes.cards[1]?.card?.card?.restaurants?.map(
+                        (res) => (
+                          <div className="md:pr-2 pb-2" key={res.info.id}>
+                            <SearchResultResCard data={res.info} />
+                          </div>
+                        )
+                      )}
+                  </div>
+                </div>
+              ) : (
+                <div>
+                  <div className="grid grid-cols-1 md:grid-cols-2 pt-4">
+                    {searchByRes.cards &&
+                      searchByRes.cards
+                        .slice(1) // Exclude the first item
+                        .filter((card, index, self) => {
+                          // Filter out duplicates based on restaurant IDs
+                          const id = card?.card?.card?.restaurant?.info?.id;
+                          return (
+                            id &&
+                            index ===
+                              self.findIndex(
+                                (c) =>
+                                  c?.card?.card?.restaurant?.info?.id === id
+                              )
+                          );
+                        })
+                        .map((res) => (
+                          <div
+                            className="pr-2 pb-2"
+                            key={res?.card?.card?.restaurant?.info?.id}
+                          >
+                            <SearchResultResCard
+                              data={res?.card?.card?.restaurant?.info}
+                            />
+                          </div>
+                        ))}
+                  </div>
+                </div>
+              )}
+            </div>
+          </ul>
+        )}
+      </div>
     </>
   );
-}
+};
 
 export default Search;
